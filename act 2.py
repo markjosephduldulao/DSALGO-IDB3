4b07742d8277969ce3fc8343d926e297ce42d6f7
markjosephduldulao <20236686@s.ubaguio.edu>
2024-08-22 16:20:38 +0800

    Add files via upload

---
+++ act 2.py
@@ -0,0 +1,5 @@
+x = int(input("enter a number"))
+sum =1
+for n in range( 1, x+1):
+  if (x % n) ==0:
+    print (n)
---
+++ main.py
@@ -0,0 +1,8 @@
+x = int(input("enter a number"))
+counter = 1
+sum = 1
+for x in range (counter,x+1):
+ sum = sum *  x
+print (sum)
+
+
